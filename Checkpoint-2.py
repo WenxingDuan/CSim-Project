f16d245bd02b3ecb37b4587addacffa8740ab558
Duan Wenxing <s1915425@ed.ac.uk>
2020-02-06 23:09:48 +0000

    branch test

--- Checkpoint-2.py
+++ Checkpoint-2.py
@@ -16,6 +16,10 @@ class Radioactive(object):
         self.n = int(n)
         self.half_life = math.log(2) / decay_constant  #real half-life
         self.average_lifetime = 1 / decay_constant
+
+
+
+
         
     def print_nuclei(self):
         for i in range(0, len(self.nucler)):
