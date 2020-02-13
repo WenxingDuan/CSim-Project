175e103d54c98ad7bfe26e0e08f2c13771202aa6
Duan Wenxing <s1915425@ed.ac.uk>
2020-02-13 20:45:38 +0000

    finished draw graph

--- Checkpoint_4.py
+++ Checkpoint_4.py
@@ -1,13 +1,14 @@
 import random
+import matplotlib.pyplot as plt
 
 
 class road(object):
     def __init__(self, carNum, roadLength, totalTime):
-        self.carNun = carNum
-        self.totalTime = totalTime
-        self.roadLength = roadLength
+        self.carNun = int(carNum)
+        self.totalTime = int(totalTime)
+        self.roadLength = int(roadLength)
         self.roadList = []
-
+        self.finalList = []
         self.roadTimeList = []
 
     def roadGenerate(self):
@@ -64,5 +65,27 @@ class road(object):
 
         return newList
 
-theRoad = road(5,10,10)
-print(theRoad.nextTime([1,1,0,1,0]))
+    def timeLoop(self):
+        self.finalList = []
+        returnList = []
+        self.finalList.append(self.roadList)
+        for i in range(self.totalTime):
+            self.finalList.append(self.nextTime(self.finalList[i]))
+
+        for i in range(len(self.finalList)):
+            returnList.append(self.finalList[len(self.finalList) - 1 - i])
+
+        self.finalList = returnList
+        return self.finalList
+
+    def drawPic(self):
+        plt.imshow(self.finalList,
+                   extent=[0, self.roadLength, 0, self.totalTime],cmap=plt.cm.gray_r)
+        
+        plt.show()
+
+
+theRoad = road(2, 5, 5)
+theRoad.roadGenerate()
+print(theRoad.timeLoop())
+theRoad.drawPic()
